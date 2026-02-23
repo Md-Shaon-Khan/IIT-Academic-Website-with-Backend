ad15fc949330e28e726b1d206c0959c106d4864a
Kuasha-19 <166922293+Kuasha-19@users.noreply.github.com>
2026-02-23 08:10:10 +0600

    db.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Database URL format: mysql+mysqlconnector://user:password@host/database_name
 # If using XAMPP/Localhost, default user is 'root' and password is empty ''
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mop1117668@localhost/dbms_project"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost/iit_portals" #password and database name check
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
