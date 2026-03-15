07c8e69cf8a3e3c9e319061252f80e4c5aece5b5
Kuasha-19 <166922293+Kuasha-19@users.noreply.github.com>
2026-03-15 20:54:10 +0600

    k

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database URL format: mysql+mysqlconnector://user:password@host/database_name
-# If using XAMPP/Localhost, default user is 'root' and password is empty ''
-database_url="mysql+mysqlconnector://root:2011@localhost:3306/IIT_Academic_website"
+# database_url="mysql+mysqlconnector://root:2011@localhost:3306/IIT_Academic_website"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mop1117668@localhost/dbms_project" #password and database name check
 
-engine = create_engine(database_url)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
